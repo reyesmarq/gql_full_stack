a1b03e001fa47bb6950ac63812f86cec48096f91
Mau Reyes <mauricio.reyes@rsmus.com>
2020-01-15 19:51:17 -0600

    login user, use jsonwebtoken

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,6 +1,13 @@
-import { Resolver, Query, Mutation, Arg } from "type-graphql"
-import { hash } from 'bcryptjs'
+import { Resolver, Query, Mutation, Arg, ObjectType, Field } from "type-graphql"
+import { hash, compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
 import { User } from "./entity/User"
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accesstoken: string
+}
 
 @Resolver()
 export class UserResolver {
@@ -14,6 +21,31 @@ export class UserResolver {
     return User.find()
   }
 
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg('email') email: string,
+    @Arg('password') password: string
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email } })
+
+    if (!user) {
+      throw new Error('Could not find user')
+    }
+    
+    const valid = await compare(password, user.password)
+    
+    if (!valid) {
+      throw new Error('Bad password')
+    }
+
+    // login successful
+
+
+    return {
+      accesstoken: sign({ userId: user.id }, 'secret', { expiresIn: '15m' })
+    }
+  }
+
   @Mutation(() => Boolean)
   async register(
     @Arg('email') email: string,
@@ -22,7 +54,7 @@ export class UserResolver {
     const hashedPassword = await hash(password, 12)
     try {
       await User.insert({
-        email, 
+        email,
         password: hashedPassword
       })
     } catch (err) {
