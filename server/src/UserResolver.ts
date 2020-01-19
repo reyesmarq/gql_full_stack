de144e775105caeb1a942e5513b2c3f3a320b349
Mau Reyes <mauricio.reyes@rsmus.com>
2020-01-19 14:42:42 -0600

    revoke refresh token

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,10 +1,11 @@
-import { Resolver, Query, Mutation, Arg, ObjectType, Field, Ctx, UseMiddleware } from "type-graphql"
+import { Resolver, Query, Mutation, Arg, ObjectType, Field, Ctx, UseMiddleware, Int } from "type-graphql"
 import { hash, compare } from 'bcryptjs'
 import { User } from "./entity/User"
 import { MyContext } from "./MyContext"
 import { createRefreshToken, createAccessToken } from "./auth"
 import { isAuth } from "./isAuth"
 import { sendRefreshtoken } from "./sendRefreshToen"
+import { getConnection } from "typeorm"
 
 @ObjectType()
 class LoginResponse {
@@ -23,7 +24,7 @@ export class UserResolver {
   @Query(() => String)
   @UseMiddleware(isAuth)
   bye(
-    @Ctx() {payload}: MyContext
+    @Ctx() { payload }: MyContext
   ) {
     console.log(payload)
     return `Your user id is: ${payload!.userId}`
@@ -33,6 +34,18 @@ export class UserResolver {
   users() {
     return User.find()
   }
+
+  @Mutation(() => Boolean)
+  async revokeRefreshTokensForUser(
+    @Arg('userId', () => Int) userId: number
+  ) {
+    await getConnection()
+      .getRepository(User)
+      .increment({ id: userId }, 'tokenVersion', 1)
+
+      return true
+  }
+
 
   @Mutation(() => LoginResponse)
   async login(
