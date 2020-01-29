1a1240baa842997eb7416282dc1f8609deec3028
Mau Reyes <mauricio.reyes@rsmus.com>
2020-01-29 08:07:27 -0600

    adding protected route

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 import { Home } from './pages/Home';
 import { Register } from './pages/Register';
 import { Login } from './pages/Login';
+import { Bye } from './pages/bye';
 
 export const Routes: React.FC = () => {
   return (
@@ -18,11 +19,15 @@ export const Routes: React.FC = () => {
           <div>
             <Link to="/login">Login</Link>
           </div>
+          <div>
+            <Link to="/bye">bye</Link>
+          </div>
         </header>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/bye" component={Bye} />
         </Switch>
       </div>
     </BrowserRouter>
--- client/src/generated/graphql.tsx
+++ client/src/generated/graphql.tsx
@@ -53,6 +53,14 @@ export type User = {
   email: Scalars['String'],
 };
 
+export type ByeQueryVariables = {};
+
+
+export type ByeQuery = (
+  { __typename?: 'Query' }
+  & Pick<Query, 'bye'>
+);
+
 export type HelloQueryVariables = {};
 
 
@@ -98,6 +106,36 @@ export type UsersQuery = (
 );
 
 
+export const ByeDocument = gql`
+    query Bye {
+  bye
+}
+    `;
+
+/**
+ * __useByeQuery__
+ *
+ * To run a query within a React component, call `useByeQuery` and pass it any options that fit your needs.
+ * When your component renders, `useByeQuery` returns an object from Apollo Client that contains loading, error, and data properties 
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useByeQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useByeQuery(baseOptions?: ApolloReactHooks.QueryHookOptions<ByeQuery, ByeQueryVariables>) {
+        return ApolloReactHooks.useQuery<ByeQuery, ByeQueryVariables>(ByeDocument, baseOptions);
+      }
+export function useByeLazyQuery(baseOptions?: ApolloReactHooks.LazyQueryHookOptions<ByeQuery, ByeQueryVariables>) {
+          return ApolloReactHooks.useLazyQuery<ByeQuery, ByeQueryVariables>(ByeDocument, baseOptions);
+        }
+export type ByeQueryHookResult = ReturnType<typeof useByeQuery>;
+export type ByeLazyQueryHookResult = ReturnType<typeof useByeLazyQuery>;
+export type ByeQueryResult = ApolloReactCommon.QueryResult<ByeQuery, ByeQueryVariables>;
 export const HelloDocument = gql`
     query Hello {
   hello
