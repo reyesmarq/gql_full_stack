795d829e7dbef34b0109a44b2c93ed9a06f06e10
Mau Reyes <mauricio.reyes@rsmus.com>
2020-01-28 21:43:04 -0600

    register user

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useRegisterMutation } from '../generated/graphql'
 
-interface  Props {
+interface Props {
 
 }
 
 export const Register: React.FC<Props> = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [register] = useRegisterMutation()
+
   return (
-    <div>Register page</div>
+    <form
+      onSubmit={async e => {
+        e.preventDefault()
+        const response = await register({
+          variables: {
+            email, password
+          }
+        })
+
+        console.log(response)
+      }}
+    >
+      <div>
+        <input
+          type="email"
+          value={email}
+          placeholder="email"
+          onChange={e => {
+            setEmail(e.target.value)
+          }}
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          value={password}
+          placeholder="password"
+          onChange={e => {
+            setPassword(e.target.value)
+          }}
+        />
+      </div>
+      <button type="submit">Register</button>
+    </form>
   )
 }
