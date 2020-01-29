1c3e32ff07c26bdeb9bdf5f66e6e7b79a252103a
Mau Reyes <mauricio.reyes@rsmus.com>
2020-01-29 10:23:16 -0600

    adding path for refresh token

--- server/src/sendRefreshToen.ts
+++ server/src/sendRefreshToen.ts
@@ -2,6 +2,8 @@ import { Response } from 'express'
 
 export const sendRefreshtoken = (res: Response, token: string) => {
   res.cookie('jid', token, {
-    httpOnly: true
+    httpOnly: true,
+    // this will send a cookie just for this path.
+    path: '/refresh_token'
   })
 }
